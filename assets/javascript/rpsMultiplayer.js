6b2d2346b17aaeeeb726f2125652dbc6bbea060d
Edward Hopper <burson.kate@gmail.com>
2019-02-02 14:48:16 -0800

    isolating key

--- assets/javascript/rpsMultiplayer.js
+++ assets/javascript/rpsMultiplayer.js
@@ -10,40 +10,39 @@
   };
   firebase.initializeApp(config);
 
-
 // define variables
 var database = firebase.database();
-
-var p1score = 0;
-var p2score = 0;
-
+var started = false;
 var count = 0;
+var player1key = '';
 var player1 = {
     name: '',
     turn: false,
     score: '',
-    rock: false,
-    paper: false,
-    scissors: false,
+    choice: '',
     diss: ''
 };
 var player2 = {
     name: '',
     turn: false,
     score: '',
-    rock: false,
-    paper: false,
-    scissors: false,
+    choice: '',
     diss: ''
+};
+
+if(started === true) {
+    $('#hello').css({'display':'none'});
 };
 
 
 $('#add-player').on('click', function() {
     event.preventDefault();
-
+    started = true;
+    
     if(count === 0) {
         var input = $('#name-input').val();     
         player1.name = input;
+        player1.turn = true;
 
         database.ref().push({
             player1: player1,
@@ -57,44 +56,72 @@ $('#add-player').on('click', function() {
             player2: player2,
         });
     }
-
 });
-
 
 database.ref().on('child_added', function(snapshot) {
     count++;
     console.log(count);
+    player1.turn = true;
+    player1key = snapshot.key;
+    console.log(snapshot.val(), snapshot.key);
 
-    console.log(snapshot.val());
     var snap = snapshot.val();
-
-    var p1name = snap.player1.name;
-    var p1score = snap.player1.score;
-
+    if(count === 1) {
+        var p1name = snap.player1.name;
+        var p1score = snap.player1.score;
+    
     $('#p1-name').text(p1name);
     $('#p1-score').text(p1score);
-
-    }, function(errorObject) {
-    console.log('The read failed: ' + errorObject.code);
-});
-
-database.ref().on('child_added', function(snapshot) {
-    console.log(count);
-
-    console.log(snapshot.val());
-    var snap = snapshot.val();
-
+    } else if (count === 2) {
     var p2name = snap.player2.name;
     var p2score = snap.player2.score;
 
     $('#p2-name').text(p2name);
     $('#p2-score').text(p2score);
+    }
+    }, function(errorObject) {
+    console.log('The read failed: ' + errorObject.code);
 
+});
+
+function player1Go() {
+    var radioValue = $('input[name="p1"]:checked').val(); 
+    player1.choice = radioValue;
+    player2.turn = true;
+
+    database.ref(player1key).update({
+        player1: player1
+    });  
+}
+
+function player2Go() {
+    var radioValue = $('input[name="p2"]:checked').val(); 
+    player2.choice = radioValue;
+
+    database.ref().update({
+        player2: player2
+    });  
+}
+
+database.ref('users').on('value', function(snapshot) {
+    console.log(snapshot);
+
+    if(player1.choice != '' && player2.choice != '') {
+        console.log('answers processed');
+    }
 
     }, function(errorObject) {
     console.log('The read failed: ' + errorObject.code);
+
 });
 
-$('#add-player').on('click', function() {
-    $('#hello').css({'display':'none'});
+$('#p1go').on('click', function() {
+    event.preventDefault();
+    player1Go();
+
+});
+
+$('#p2go').on('click', function() {
+    event.preventDefault();
+    player2Go()
 });
