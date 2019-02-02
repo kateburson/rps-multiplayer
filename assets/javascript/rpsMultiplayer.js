e011db33f41f8a70ca343a9e0fee9090099b639c
Edward Hopper <burson.kate@gmail.com>
2019-02-01 16:57:29 -0800

    starting game portion

--- assets/javascript/rpsMultiplayer.js
+++ assets/javascript/rpsMultiplayer.js
@@ -18,49 +18,83 @@ var p1score = 0;
 var p2score = 0;
 
 var count = 0;
+var player1 = {
+    name: '',
+    turn: false,
+    score: '',
+    rock: false,
+    paper: false,
+    scissors: false,
+    diss: ''
+};
+var player2 = {
+    name: '',
+    turn: false,
+    score: '',
+    rock: false,
+    paper: false,
+    scissors: false,
+    diss: ''
+};
+
 
 $('#add-player').on('click', function() {
     event.preventDefault();
 
     if(count === 0) {
-        var name = $('#name-input').val();
-        var player1 = name;       
-        
+        var input = $('#name-input').val();     
+        player1.name = input;
+
         database.ref().push({
             player1: player1,
         });
 
-
     } else if(count === 1 && count < 2) {
-        var name = $('#name-input').val();
-        var player2 = name;       
-        
+        var input = $('#name-input').val();     
+        player2.name = input;
+
         database.ref().push({
             player2: player2,
         });
-
     }
 
 });
 
+
 database.ref().on('child_added', function(snapshot) {
-    console.log(snapshot.val());
     count++;
     console.log(count);
 
-    // var p1name = snapshot.val().player1[0];
-    // var p1fact = snapshot.val().player1[1];
-    // var p2name = snapshot.val().player2[0];
-    // var p2fact = snapshot.val().player2[1];
+    console.log(snapshot.val());
+    var snap = snapshot.val();
 
-    // $('#p1-name').text(p1name);
-    // $('#p1-fact').text(p1fact);
-    // $('#p2-name').text(p2name);
-    // $('#p2-fact').text(p2fact);
+    var p1name = snap.player1.name;
+    var p1score = snap.player1.score;
+
+    $('#p1-name').text(p1name);
+    $('#p1-score').text(p1score);
 
     }, function(errorObject) {
     console.log('The read failed: ' + errorObject.code);
 });
 
+database.ref().on('child_added', function(snapshot) {
+    console.log(count);
+
+    console.log(snapshot.val());
+    var snap = snapshot.val();
+
+    var p2name = snap.player2.name;
+    var p2score = snap.player2.score;
+
+    $('#p2-name').text(p2name);
+    $('#p2-score').text(p2score);
 
 
+    }, function(errorObject) {
+    console.log('The read failed: ' + errorObject.code);
+});
+
+$('#add-player').on('click', function() {
+    $('#hello').css({'display':'none'});
+});
