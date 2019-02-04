21cbeb231fbf8ac94c19842107d4c838b9d34af6
Edward Hopper <burson.kate@gmail.com>
2019-02-03 21:27:07 -0800

    hw submission with readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# rps-multiplayer
+# Rock. Paper. Diss
+
+### Overview
+
+This is a rock-paper-scissors style game where there is the ability to send a 'diss' to the oponent insead of a scissors option. The game allows for two players at a time and is written in javascript and jQuery with data stored in the Firebase realtime database.
--- assets/javascript/rpsMultiplayer.js
+++ assets/javascript/rpsMultiplayer.js
@@ -14,6 +14,8 @@
 var database = firebase.database();
 
 var started = false;
+var p1clicked = false;
+var p2clicked = false;
 var count = 0;
 
 var player1key = '';
@@ -36,6 +38,7 @@ var player2 = {
 
 
 $('#add-player').on('click', function() {
+
     event.preventDefault();
     started = true;
     player1.turn = true;
@@ -62,25 +65,24 @@ $('#add-player').on('click', function() {
 
 database.ref().on('child_added', function(snapshot) {
     count++;
-    // console.log(count);
+    console.log(count);
     console.log(snapshot.val(), snapshot.key);
 
-    var snap = snapshot.val();
     if(count === 1) {
         player1key = snapshot.key;
-        var p1name = snap.player1.name;
-        var p1score = snap.player1.score;
+        var p1name = player1.name;
+        var p1score = player1.score;
     
         $('#p1-name').text(p1name);
-        $('#p1-score').text(p1score);
+        $('#p1-score').text('Score: ' + p1score);
 
     } else if(count === 2) {
     player2key = snapshot.key;
-    var p2name = snap.player2.name;
-    var p2score = snap.player2.score;
+    var p2name = player2.name;
+    var p2score = player2.score;
 
     $('#p2-name').text(p2name);
-    $('#p2-score').text(p2score);
+    $('#p2-score').text('Score: ' + p2score);
     }
 
     }, function(errorObject) {
@@ -92,27 +94,54 @@ function player1Go() {
     var radioValue = $('input[name="p1"]:checked').val(); 
     player1.choice = radioValue;
     console.log(player1.choice);
-
-    database.ref(player1key).update({
-        player1: player1
-    });
 };
 
 function player2Go() {
     var radioValue = $('input[name="p2"]:checked').val(); 
     player2.choice = radioValue;
     console.log(player2.choice);
-
-    database.ref(player2key).update({
-        player2: player2
-    });
     // player1.turn = true;
 };
 
-
+function winner() {
+    if(player1.choice === 'rock' && player2.choice === 'paper') {
+        player2.score++;
+        
+    } else if(player1.choice === 'paper' && player2.choice === 'rock') {
+        player1.score++;
+        database.ref().update({
+            player1:player1.score
+        })
+    } else if(player1.choice === 'paper' && player2.choice === 'diss') {
+        player2.score++;
+        database.ref().update({
+            player2:player2.score
+        })
+    } else if(player1.choice === 'diss' && player2.choice === 'paper') {
+        player1.score++;
+        database.ref().update({
+            player1:player1.score
+        })
+    } else if(player1.choice === 'diss' && player2.choice === 'rock') {
+        player2.score++;
+        database.ref().update({
+            player2:player2.score
+        })
+    } else if(player1.choice === 'rock' && player2.choice === 'diss') {
+        player1.score++;
+        database.ref().update({
+            player1:player1.score
+        })
+    } else if(player1.choice === player2.choice) {
+        player1.score = player1.score;
+        player2score = player2.score;
+    }
+};
+ 
 $('#p1go').on('click', function() {
     event.preventDefault();
     player1Go();
+    p1clicked =  true;
     // player1.turn = false;
     // player2.turn = true;
 });
@@ -120,62 +149,40 @@ $('#p1go').on('click', function() {
 $('#p2go').on('click', function() {
     event.preventDefault();
     player2Go();
+    p2clicked = true;
     // player1.turn = true;
     // player2.turn = false;
 });
 
-// if(player2.choice != '') {
-database.ref().on('value', function(snapshot) {
+if(p1clicked === true && p2clicked === true) {
+    database.ref(player2key).update({
+        player2: player2
+    });
+
+    database.ref(player1key).update({
+        player1: player1
+    });
+};
+
+database.ref().on('child_changed', function(snapshot) {
+    console.log('data read');
     var snap = snapshot.val();
-    console.log(snap);
-    var p1choice = snap.player1.choice;
-    var p2choice = snap.player2.choice;
 
-    if(p1choice === 'rock' && p2choice === 'paper') {
-        player2.score++;
-        database.ref(player2key).update({
-            player2: player2,
-        });  
-    } else if(p1choice === 'paper' && p2choice === 'rock') {
-        player1.score++;
-        database.ref(player1key).update({
-            player1: player1,
-        }); 
-    } else if(p1choice === 'paper' && p2choice === 'diss') {
-        player2.score++;
-        database.ref(player2key).update({
-            player2: player2,
-        });  
-    } else if(p1choice === 'diss' && p2choice === 'paper') {
-        player1.score++;
-        database.ref(player1key).update({
-            player1: player1,
-        }); 
-    } else if(p1choice === 'diss' && p2choice === 'rock') {
-        player2.score++;
-        database.ref(player2key).update({
-            player2: player2,
-        });  
-    } else if(p1choice === 'rock' && p2choice === 'diss') {
-        player1.score++;
-        database.ref(player1key).update({
-            player1: player1,
-        }); 
-    } else if(p1choice === p2choice) {
-        player1.score = player1.score;
-        player2score = player2.score;
-    }
+    $('#p1-choice').text(snap.player1.choice);
+    $('#p1-score').text(snap.player1.score);
+    $('#p2-choice').text(snap.player2.choice);
+    $('#p2-score').text(snap.player2.score);
 
-    // var p1score = player1.score;
-    // $('#p1-score').text(p1score);
 
-    // var p2score = player2.score;
-    // $('#p2-score').text(p2score);
-
+    
     }, function(errorObject) {
     console.log('The read failed: ' + errorObject.code);
 });
-// }
+    
+
+
+
+
 
 
 
