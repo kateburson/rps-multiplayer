7af0eb038198a6af96eaab40ed75afacf809c8fd
Edward Hopper <burson.kate@gmail.com>
2019-02-17 20:34:04 -0800

    restructuring

--- assets/javascript/rpsMultiplayer.js
+++ assets/javascript/rpsMultiplayer.js
@@ -27,7 +27,7 @@ var player1 = {
     score: 0,
     choice: '',
     diss: '',
-    clicked: ''
+    clicked: false
 };
 var player2 = {
     name: '',
@@ -35,161 +35,218 @@ var player2 = {
     score: 0,
     choice: '',
     diss: '',
-    clicked: ''
+    clicked: false
 };
 
+var playerKeys = [];
+
+function initializeUser() {
+    var name = $('#name-input').val();
+
+    var entry = database.ref().push({
+        'name': name,
+        'score': 0,
+        'choice': null
+    })
+    playerKeys.push(entry.key);
+};
 
 $('#add-player').on('click', function() {
-
     event.preventDefault();
-    started = true;
-    player1.turn = true;
-
-    if(started === true) {
-        $('#hello').css({'display':'none'});
-    }
-    if(count === 0) {
-        var input = $('#name-input').val();     
-        player1.name = input;
-
-        database.ref().push({
-            player1: player1,
-        });
-    } else if(count === 1 && count < 2) {
-        var input = $('#name-input').val();     
-        player2.name = input;
-
-        database.ref().push({
-            player2: player2,
-        });
-    }
+    initializeUser();
 });
 
-database.ref().on('child_added', function(snapshot) {
-    count++;
-    console.log(count);
-    console.log(snapshot.val(), snapshot.key);
+// $('#add-player').on('click', function() {
 
-    if(count === 1) {
-        player1key = snapshot.key;
-        var p1name = snapshot.val().player1.name;
-        var p1score = snapshot.val().player1.score;
+//     event.preventDefault();
+//     started = true;
+//     player1.turn = true;
+
+//     if(started === true) {
+//         $('#hello').css({'display':'none'});
+//     }
+//     if(count === 0) {
+//         var input = $('#name-input').val();     
+//         player1.name = input;
+
+//         database.ref().push({
+//             player1: player1,
+//         });
+//     } else if(count === 1 && count < 2) {
+//         var input = $('#name-input').val();     
+//         player2.name = input;
+
+//         database.ref().push({
+//             player2: player2,
+//         });
+//     }
+// });
+
+
+
+// database.ref().on('child_added', function(snapshot) {
+//     count++;
+//     console.log(count);
+//     console.log(snapshot.val(), snapshot.key);
+
+//     if(count === 1) {
+//         player1key = snapshot.key;
+//         var p1name = snapshot.val().player1.name;
+//         var p1score = snapshot.val().player1.score;
     
-        $('#p1-name').text(p1name);
-        $('#p1-score').text('Score: ' + p1score);
+//         $('#p1-name').text(p1name);
+//         $('#p1-score').text('Score: ' + p1score);
 
-    } else if(count === 2) {
-    player2key = snapshot.key;
-    var p2name = snapshot.val().player2.name;
-    var p2score = snapshot.val().player2.score;
+//     } else if(count === 2) {
+//     player2key = snapshot.key;
+//     var p2name = snapshot.val().player2.name;
+//     var p2score = snapshot.val().player2.score;
 
-    $('#p2-name').text(p2name);
-    $('#p2-score').text('Score: ' + p2score);
-    }
+//     $('#p2-name').text(p2name);
+//     $('#p2-score').text('Score: ' + p2score);
+//     }
 
-    }, function(errorObject) {
-    console.log('The read failed: ' + errorObject.code);
+//     }, function(errorObject) {
+//     console.log('The read failed: ' + errorObject.code);
 
-});
+// });
 
 function player1Go() {
-    var radioValue = $('input[name="p1"]:checked').val(); 
-    player1.choice = radioValue;
-    console.log(player1.choice);
-    player1.clicked =  true;
+    // console.log(database.ref(player1key))
+    // if (player1.clicked === true) {
+    //     return
+    // }
+    // var radioValue = $('input[name="p1"]:checked').val(); 
+    // player1.choice = radioValue;
+    // console.log(player1.choice);
+    // player1.clicked =  true;
+
+    // database.ref(player1key).update({
+    //     player1: player1
+    // });
 
     database.ref(player1key).update({
-        player1: player1
+       choice: $('input[name="p1"]:checked').val(),
     });
 };
 
 function player2Go() {
-    var radioValue = $('input[name="p2"]:checked').val(); 
-    player2.choice = radioValue;
-    console.log(player2.choice);
-    player2.clicked = true;
+    // if (player2.clicked === true) {
+    //     return
+    // }
+    // var radioValue = $('input[name="p2"]:checked').val(); 
+    // player2.choice = radioValue;
+    // console.log(player2.choice);
+    // player2.clicked = true;
+
+    // database.ref(player2key).update({
+    //     player2: player2
+    // });
 
     database.ref(player2key).update({
-        player2: player2
+        choice: $('input[name="p2"]:checked').val(),
     });
 };
 
-function winner(playerLeft, playerRight) {
+// function winner(playerLeft, playerRight) {
+//     if(playerLeft.choice === 'rock' && playerRight.choice === 'paper') {
+//         playerRight.score++;
+//         database.ref().update({
+//             player2:player2.score
+//         })
+//     } else if(playerLeft.choice === 'paper' && playerRight.choice === 'rock') {
+//         playerLeft.score++;
+//         database.ref().update({
+//             player1:player1.score
+//         })
+//     } else if(playerLeft.choice === 'paper' && playerRight.choice === 'diss') {
+//         playerRight.score++;
+//         database.ref().update({
+//             player2:player2.score
+//         })
+//     } else if(playerLeft.choice === 'diss' && playerRight.choice === 'paper') {
+//         playerLeft.score++;
+//         database.ref().update({
+//             player1:player1.score
+//         })
+//     } else if(playerLeft.choice === 'diss' && playerRight.choice === 'rock') {
+//         playerRight.score++;
+//         database.ref().update({
+//             player2:player2.score
+//         })
+//     } else if(playerLeft.choice === 'rock' && playerRight.choice === 'diss') {
+//         playerLeft.score++;
+//         database.ref().update({
+//             player1:player1.score
+//         })
+//     } else if(playerLeft.choice === playerRight.choice) {
+//         playerLeft.score = playerLeft.score;
+//         playerRight.score = playerRight.score;
+//     }
+// };
+
+function playGame(playerLeft, playerRight) {
     if(playerLeft.choice === 'rock' && playerRight.choice === 'paper') {
-        playerRight.score++;
-        database.ref().update({
-            player2:player2.score
-        })
+        return(playerRight);
     } else if(playerLeft.choice === 'paper' && playerRight.choice === 'rock') {
-        playerLeft.score++;
-        database.ref().update({
-            player1:player1.score
-        })
+        return(playerLeft);
     } else if(playerLeft.choice === 'paper' && playerRight.choice === 'diss') {
-        playerRight.score++;
-        database.ref().update({
-            player2:player2.score
-        })
+        return(playerRight);
     } else if(playerLeft.choice === 'diss' && playerRight.choice === 'paper') {
-        playerLeft.score++;
-        database.ref().update({
-            player1:player1.score
-        })
+        return(playerLeft);
     } else if(playerLeft.choice === 'diss' && playerRight.choice === 'rock') {
-        playerRight.score++;
-        database.ref().update({
-            player2:player2.score
-        })
-    } else if(snap[player1key].player1.choice === 'rock' && playerRight.choice === 'diss') {
-        playerLeft.score++;
-        database.ref().update({
-            player1:player1.score
-        })
+        return(playerRight);
+    } else if(playerLeft.choice === 'rock' && playerRight.choice === 'diss') {
+        return(playerLeft);
     } else if(playerLeft.choice === playerRight.choice) {
-        playerLeft.score = playerLeft.score;
-        playerRight.score = playerRight.score;
+        return({});
     }
 };
  
 $('#p1go').on('click', function() {
     event.preventDefault();
-    play();
-    // if(player1.turn === true) {
-    //     player1Go();
-    // }
-    // player1.turn = false;
-    // player2.turn = true;
+    player1Go();
 });
 
 $('#p2go').on('click', function() {
     event.preventDefault();
-    play();
-    // if(player2.turn === true) {
-    //     player2Go();
-    // }
-    // player1.turn = true;
-    // player2.turn = false;
+    player2Go();
 });
 
+// database.ref().on('value', function(snapshot) {
+//     console.log('data read');
+//     var snap = snapshot.val();
+//     var player1Db = snap[player1key].player1;
+//     var player2Db = snap[player2key].player2;
+//     if(player1Db.clicked === true && player2Db.clicked === true) {
+//         winner(player1Db, player2Db);
+//         $('#p1-choice').text(player1Db.choice);
+//         $('#p1-score').text(player1Db.score);
+//         $('#p2-choice').text(player2Db.choice);
+//         $('#p2-score').text(player2Db.score);
+//         player1.clicked = false;
+//         player2.clicked = false;
+//         player1Db.clicked = false;
+//         player2Db.clicked = false;
+//         database.ref().update({
+//             player1:player1,
+//             player2:player2
+//         })
+//     } 
+//     },
+//     function(errorObject) {
+//         console.log('The read failed: ' + errorObject.code);
+//     }
+// );
+
 database.ref().on('value', function(snapshot) {
-    console.log('data read');
+    return
     var snap = snapshot.val();
-    var player1 = snap[player1key].player1;
-    var player2 = snap[player2key].player2;
-    console.log(snapshot, snap);
-    console.log(player1.clicked);
-    console.log(player2.clicked);
-    if(player1.clicked === true && player2.clicked === true) {
-        winner(player1, player2);
-        $('#p1-choice').text(player1.choice);
-        $('#p1-score').text(player1.score);
-        $('#p2-choice').text(player2.choice);
-        $('#p2-score').text(player2.score);
+    var player1Db = snap[player1key].player1;
+    var player2Db = snap[player2key].player2;
+    console.log(player1Db, player2Db);
+    if(player1Db.choice != null && player2Db.choice != null) {
+        var winner = playGame(player1Db, player2Db);
+        // if(winner.name === )
     } 
-    player1.clicked = false;
-    player2.clicked = false;
-    },
-    function(errorObject) {
-        console.log('The read failed: ' + errorObject.code);
-    }
-);
+});
